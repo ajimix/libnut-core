f467476cb0abccc1badc0cb28c6bf8ab6476f08a
Adria <git@adria.link>
2025-10-15 23:09:25 +0800

    fix: publish scoped package with public access

--- .build/pre-release.sh
+++ .build/pre-release.sh
@@ -9,4 +9,4 @@ nextVersion=${patchVersion}-next."$(date +%Y%m%d%H%M%S)"
 echo "${nextVersion:1}"
 
 npm version --no-git-tag -f "${nextVersion:1}"
-npm run publish:next
+npm publish --tag next --access public
